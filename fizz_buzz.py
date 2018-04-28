599c34186c0dff907758219ec4589506835fdc63
ElaineSRamos <elainesramos@gmail.com>
2018-04-27 22:58:34 -0300

    versao com for

--- fizz_buzz.py
+++ fizz_buzz.py
@@ -4,14 +4,26 @@
 # instead of the number and for the multiples of five print "Buzz". For numbers which are multiples of both three and five print "FizzBuzz".
 
 
-i = 1
-while (i <=100):
-    if (i % 3 == 0) and (i % 5 == 0):
-        print(i, ' - FizzBuzz')
-    elif (i % 3== 0):
-        print(i, ' - Fizz')
-    elif (i % 5 == 0):
-        print(i , ' - Buzz')
-    else :
-        print(i,'Nao é multiplo de 3 nem 5')
-    i += 1
+# i = 1
+# while (i <=100):
+#     if (i % 3 == 0) and (i % 5 == 0):
+#         print(i, ' - FizzBuzz')
+#     elif (i % 3== 0):
+#         print(i, ' - Fizz')
+#     elif (i % 5 == 0):
+#         print(i , ' - Buzz')
+#     else :
+#         print(i,'Nao é multiplo de 3 nem 5')
+#     i += 1
+
+
+
+for v in range(100+1):
+    if v % 3 == 0 and  v % 5 == 0:
+        print(v, ' - FizzBuzz')
+    elif v % 3 == 0:
+        print(v, ' - Fizz')
+    elif v % 5 == 0:
+        print(v, ' - Buzz')
+    else:
+        print(v,'Nao é multiplo de 3 nem 5')
